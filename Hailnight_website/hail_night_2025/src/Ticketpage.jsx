04c05d672f9f6027e96126ad3ec37c4eea8bd72f
ืnoppadol-amo <noppadol.amo@gmail.com>
2025-01-20 14:02:59 +0700

    Update to ticket
    from 6 baan to baan กลาง

--- Hailnight_website/hail_night_2025/src/Ticketpage.jsx
+++ Hailnight_website/hail_night_2025/src/Ticketpage.jsx
@@ -10,113 +10,71 @@ export default function TicketPage() {
       </div>
 
       {/* Ticket Page Content */}
-<div className="flex flex-col items-center justify-center flex-grow gap-8 text-[#f3ead9] pt-[60px]">
+<div className="flex flex-col items-center justify-center flex-grow gap-16 text-[#f3ead9] pt-[60px]">
 <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold font-csRoger text-[#fccf7f] text-center">
           Ticket
         </h1>
   {/* First row of buttons */}
-  <div className="flex flex-wrap gap-8 justify-center w-full">
-  <button 
-    onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSee0Vv4eb3XB7Ve9-wMTrZAOa5O4leUzUjhHjmtGWpv0nZqmg/viewform?usp=header', '_blank')}
-    className="relative flex w-[90%] sm:w-[914px] max-w-full h-[369px] items-center justify-center gap-2.5 px-6 sm:px-[247px] py-6 sm:py-[71px] rounded-[20px] hover:scale-105 transition-all duration-300"
+  <div className="flex flex-col sm:flex-row gap-8 items-center justify-center w-full ">
+  {/* Scaled Image */}
+  <div className="relative w-[500px] sm:w-[500px] max-w-full h-[550px] rounded-[20px] px-4 ">
+    <img
+      src="src\pic\S__10420271.jpg"
+      alt="Scaled Image"
+      className="w-full h-full rounded-[20px] "
+    />
+  </div>
+
+  {/* Button */}
+  
+  
+  <div 
+    className="relative flex w-[90%] sm:w-[700px] max-w-full h-[650px] sm:h-[800px] border-2 border-gray-100 rounded-[20px] overflow-hidden transition-all duration-300"
   >
     {/* Background Image */}
     <img 
       src="src\pic\canon-poon-hailnight-659.jpg"
-      alt="Baan กลาง"
-      className="absolute inset-0 w-full h-full object-cover opacity-70 brightness-75 rounded-[20px] hover:opacity-100 transition-all duration-300"
+      alt="Background"
+      className="absolute inset-0 w-full h-full object-cover blur  brightness-50  "
     />
 
-    {/* Text */}
-    <div className="relative z-10 w-[396px] font-csRoger font-normal text-[#f3ead9] text-[64px] text-center tracking-[0] leading-[89.6px]">
-      BAAN กลาง
+    {/* Overlay Content */}
+    <div className="absolute top-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-8 z-20">
+      {/* Text */}
+      <div className="font-csRoger font-bold text-[#fccf7f] text-[64px] text-center leading-[89.6px] whitespace-nowrap">
+        GET TICKET
+      </div>
+
+      {/* Ticket Image */}
+      <img
+        src="src\pic\ticket.PNG"
+        alt="Ticket"
+        className="w-auto  sm:max-w-[150%]"
+      />
+      <div className="font-csRoger font-bold text-[#fccf7f] text-[42px] text-center leading-[89.6px] whitespace-nowrap">
+        Early Bird - 379 Bath
+      </div>
+       <a 
+  href="https://docs.google.com/forms/d/e/1FAIpQLSee0Vv4eb3XB7Ve9-wMTrZAOa5O4leUzUjhHjmtGWpv0nZqmg/viewform?usp=header"  // Replace with your URL
+  target="_blank"  // Open link in a new tab
+  rel="noopener noreferrer"  // Security for opening in a new tab
+>
+  <div className="bg-[#f9d490] flex items-center justify-center gap-2.5 px-[100px] py-6 relative rounded-[20px] mb-[40px] hover:scale-110 transition-all duration-300">
+    <div className="relative w-fit font-csRosger font-bold text-[#4a381f] text-[36px] text-center tracking-[0] leading-[50px] underline whitespace-nowrap">
+      Buy Now
     </div>
-  </button>
+  </div>
+</a>
+    </div>
+  </div>
+
+
 </div>
 
 
 
-  {/* Second row of buttons */}
-  <div className="flex flex-wrap gap-8 justify-center w-full">
-    <button 
-    onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSdNLiblmeNYn0po8RNwpusHphFgqWNBY67_7Geq-20NmpDwcA/viewform?usp=header','_blank')}
-    className="flex w-[90%] sm:w-[440px] max-w-full h-[298px] items-center justify-center gap-2.5 sm:px-[53px] sm:py-[52px] relative  rounded-[20px]  hover:scale-105 transition-all duration-300 ">
-      <img 
-      src ="src\pic\baan 1.JPG"
-      className="absolute inset-0 bg-[#758fb6] opacity-70 brightness-75 rounded-[20px] hover:bg-[#5a6f8e] hover:opacity-100 transition-all duration-300"></img>
 
-      <div className="relative w-[333px] font-csRoger font-normal text-[#f3ead9] text-5xl text-center tracking-[0] leading-[67.2px]">
-        BAAN 1
-      </div>
-    </button>
-
-    <button 
-    onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSemyA8mNNRz_VIiW86l1HvQba9lTT5_FPHvR6MJ5c1o2zRPsw/viewform?usp=header','_blank')}
-    className="flex w-[90%] sm:w-[440px] max-w-full h-[298px] items-center justify-center gap-2.5 sm:px-[53px] sm:py-[52px] relative  rounded-[20px]  hover:scale-105 transition-all duration-300">
-      <img 
-      src ="src\pic\DSC02570.JPG"
-      className="absolute inset-0 bg-[#758fb6] opacity-70 brightness-75 rounded-[20px] hover:bg-[#5a6f8e] hover:opacity-100 transition-all duration-300"></img>
-
-      <div className="relative w-[334px] font-csRoger font-normal text-[#f3ead9] text-5xl text-center tracking-[0] leading-[67.2px]">
-        BAAN 2
-      </div>
-    </button>
-  </div>
-
-  {/* Third row of buttons */}
-  <div className="flex flex-wrap gap-8 justify-center w-full">
-    <button 
-    onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSeyGiO0aO3s6g2okIVaxA1yktdLi_8M-kedqtZCLH7gsLZeEw/viewform?usp=header','_blank')}
-    className="flex w-[90%] sm:w-[440px] max-w-full h-[298px] items-center justify-center gap-2.5 sm:px-[53px] sm:py-[52px] relative  rounded-[20px]  hover:scale-105 transition-all duration-300">
-      <img 
-      src ="src\pic\DSC02506.JPG"
-      className="absolute inset-0 bg-[#758fb6] opacity-70 brightness-75 rounded-[20px] hover:bg-[#5a6f8e] hover:opacity-100 transition-all duration-300"></img>
-
-
-      <div className="relative w-[333px] font-csRoger font-normal text-[#f3ead9] text-5xl text-center tracking-[0] leading-[67.2px]">
-        BAAN 3
-      </div>
-    </button>
-
-    <button 
-    onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSeyxcTT_4llDFEjo6dqhwfmvWL2gMytpYrG7KBxjIpHdPio1A/viewform?usp=header','_blank')}
-    className="flex w-[90%] sm:w-[440px] max-w-full h-[298px] items-center justify-center gap-2.5 sm:px-[53px] sm:py-[52px] relative  rounded-[20px]  hover:scale-105 transition-all duration-300">
-      <img 
-      src ="src\pic\DSC02586.JPG"
-      className="absolute inset-0 bg-[#758fb6] opacity-70 brightness-75 rounded-[20px] hover:bg-[#5a6f8e] hover:opacity-100 transition-all duration-300"></img>
-
-      <div className="relative w-[334px] font-csRoger font-normal text-[#f3ead9] text-5xl text-center tracking-[0] leading-[67.2px]">
-        BAAN 4
-      </div>
-    </button>
-  </div>
-
-  {/* Fourth row of buttons */}
-  <div className="flex flex-wrap gap-8 justify-center w-full">
-    <button 
-    onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSdywHNrPL9uamJob-0DwRgpx1rIytEBfvcdCHk_o0OMF7z90w/viewform?usp=header','_blank')}
-    className="flex w-[90%] sm:w-[440px] max-w-full h-[298px] items-center justify-center gap-2.5 sm:px-[53px] sm:py-[52px] relative  rounded-[20px]  hover:scale-105 transition-all duration-300">
-      <img 
-      src ="src\pic\DSC02864.JPG"
-      className="absolute inset-0 bg-[#758fb6] opacity-70 brightness-75 rounded-[20px] hover:bg-[#5a6f8e] hover:opacity-100 transition-all duration-300"></img>
-
-      <div className="relative w-[333px] font-csRoger font-normal text-[#f3ead9] text-5xl text-center tracking-[0] leading-[67.2px]">
-        BAAN 5
-      </div>
-    </button>
-
-    <button 
-     
-    className="flex w-[90%] sm:w-[440px] max-w-full h-[298px] items-center justify-center gap-2.5 sm:px-[53px] sm:py-[52px] relative  rounded-[20px]  hover:scale-105 transition-all duration-300">
-      <img 
-      src ="src\pic\DSC02525.JPG"
-      className="absolute inset-0 bg-[#758fb6] opacity-70 brightness-75 rounded-[20px]  hover:opacity-100 transition-all duration-300"></img>
-
-      <div className="relative w-[333px] font-csRoger font-normal text-[#f3ead9] text-5xl text-center tracking-[0] leading-[67.2px]">
-        BAAN 6
-      </div>
-    </button>
-  </div>
+ 
 </div>
 
 {/* New Section */}
